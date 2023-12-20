486a589ba76b3c5a0b3e3991d7bfb0cdd7e69a27
Sanjay.R <133894028+Sanjayr4@users.noreply.github.com>
2023-12-20 18:59:30 +0530

    Update tips .c

--- tips .c
+++ tips .c
@@ -3,4 +3,5 @@
         printf (",");
     }
 // i = for loop range; 
-// thise is used to avoid printing of the , in the last of the running loop
+// thise is used to avoid printing of the , in the last of the running loop;
+// LENGTH array size;
